be2a3795c174b9fae6e9348ef3fe7b53286fd948
Wojciech Zawistowski <wojciech.zawistowski@gmail.com>
2014-02-26 20:20:10 +0100

    FIX: quotes convention normalization (changes quotes in Karma config from single to double like in the rest of the project)

--- karma.conf.js
+++ karma.conf.js
@@ -1,24 +1,24 @@
 module.exports = function(config) {
   config.set({
 
-    browsers: ['PhantomJS'],
+    browsers: ["PhantomJS"],
 
-    frameworks: ['qunit'],
+    frameworks: ["qunit"],
 
     preprocessors: {
-      'app/templates/*.hbs': 'ember'
+      "app/templates/*.hbs": "ember"
     },
 
     files: [
-      'bower_components/jquery/jquery.js',
-      'bower_components/handlebars/handlebars.js',
-      'bower_components/ember/ember.js',
-      'app/templates/*.hbs',
-      'app/app.js',
-      'app/**/*.js',
-      'test/test_helper.js',
-      'test/unit/**/*_test.js',
-      'test/e2e/**/*_test.js'
+      "bower_components/jquery/jquery.js",
+      "bower_components/handlebars/handlebars.js",
+      "bower_components/ember/ember.js",
+      "app/templates/*.hbs",
+      "app/app.js",
+      "app/**/*.js",
+      "test/test_helper.js",
+      "test/unit/**/*_test.js",
+      "test/e2e/**/*_test.js"
     ],
 
     autoWatch: true
