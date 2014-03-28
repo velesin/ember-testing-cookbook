94cdcfaf9335031be6e001339b0b728d8fb797ab
Wojciech Zawistowski <wojciech.zawistowski@gmail.com>
2014-03-28 20:37:49 +0100

    templates - view via container example

---
+++ test/unit/templates/views_test.js
@@ -0,0 +1,21 @@
+module("templates - views", {
+  setup: function() {
+    var container = new Ember.Container();
+    container.register("view:inner", Ember.View.extend({
+      template: Ember.Handlebars.compile("view stub")
+    }));
+
+    var view = Ember.View.create({
+      container: container,
+      template: Ember.TEMPLATES["unit-views"]
+    });
+
+    Ember.run(function() {
+      view.appendTo("#qunit-fixture");
+    });
+  }
+});
+
+test("template renders correctly", function() {
+  equal(Ember.$("#qunit-fixture .unit-views").text(), "view stub");
+});
