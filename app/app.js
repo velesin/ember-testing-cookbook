de432e55bcadbdd1d85ad3be0b8ba5b2798b5890
Wojciech Zawistowski <wojciech.zawistowski@gmail.com>
2014-04-07 20:09:48 +0200

    moved all the app parts for the basic e2e example to the test file

--- app/app.js
+++ app/app.js
@@ -1,5 +1,1 @@
 App = Ember.Application.create();
-
-App.Router.map(function() {
-  this.route("e2e-basic");
-});
--- test/e2e/basic_test.js
+++ test/e2e/basic_test.js
@@ -1,3 +1,14 @@
+
+Ember.TEMPLATES["e2e-basic"] = Ember.Handlebars.compile(
+  '<div id="e2e-basic">e2e basic page text</div>'
+);
+
+App.Router.map(function() {
+  this.route("e2e-basic");
+});
+
+//--------------------------------------------------------------------------
+
 module("e2e-basic page", {
   setup: function() {
     App.reset();
